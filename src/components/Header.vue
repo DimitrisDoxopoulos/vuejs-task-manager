d2c2e3c82e5206307196f2eb28a021988c983050
Dimitris Doxopoulos <olonir@hotmail.com>
2021-07-05 08:17:10 +0300

    adding project files and folders

---
+++ src/components/Header.vue
@@ -0,0 +1,48 @@
+<!-- ----------------- Templates --------------- -->
+
+<template>
+    <header>
+        <h1> {{ title }} </h1>
+        <Button
+            @btn-click="$emit('toggle-add-task')" 
+            :text="showAddTask ? 'Close' : 'Add Task'"
+            :color="showAddTask ? 'red' : 'green'"
+        />
+    </header>
+</template>
+    
+<!-- ----------------- Templates --------------- -->
+
+<!-- ----------------- Scipts --------------- -->
+<script>
+    import Button from './button.vue'
+
+    export default
+    {
+        name: 'Header',
+        
+        props: {
+            title: String,
+            showAddTask: Boolean,
+        },
+
+        components: {
+            Button
+        }
+    }
+</script>
+
+<!-- -------x--------- Scipts --------x------- -->
+
+<!-- ----------------- Styles --------------- -->
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
+
+<!-- ------x--------- Styles --------x----- -->
---
+++ src/components/button.vue
@@ -0,0 +1,34 @@
+<!-- ----------------- Templates --------------- -->
+
+<template>
+    <button :style="{background: color}" class="btn" @click='onClick()'>{{ text }}</button>
+</template>
+    
+<!-- ----------------- Templates --------------- -->
+
+<!-- ----------------- Scipts --------------- -->
+<script>
+
+    export default
+    {
+        name: 'Button',
+        props: {
+            text: String,
+            color: String
+        },
+        methods: {
+            onClick() {
+                this.$emit('btn-click')
+            },
+        }
+    
+    }
+</script>
+
+<!-- -------x--------- Scipts --------x------- -->
+
+<!-- ----------------- Styles --------------- -->
+
+
+
+<!-- ------x--------- Styles --------x----- -->
